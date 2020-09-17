4d5e92f93346ab25b5126ea5307d5b0e5700314a
Merlin <batinicajames@gmail.com>
2020-09-17 15:05:43 -0700

    finished ttt

--- scripts.js
+++ scripts.js
@@ -1,75 +1,134 @@
-let factory = () => {
-    let i = true;
-      increment = () => {
-        i = true
-        return i
-    };
-      decrement = () => {
-        i = false
-        return i
-      }
-      getI = () => {
-        return i
-      }  
-  return {getI, increment, decrement}
+let players = () => {
+  let play = {
+    truth : true,
+    red : 0,
+    blue : 0,
+    ties : 0
+   }
+  let incrementRed = () => {
+    play.red++;
   }
-  const player = factory()
-  let cellClass = document.getElementsByClassName('grid-cell')
-  let selectedTd;
+  let incrementBlue = () => {
+    play.blue++
+  }
+  let ties = () => {
+    play.ties++
+  }
+  let falsey = () => {
+    play.truth = false;
+  }
+  let truey = () => {
+    play.truth = true;
+  }
   
-  function gameplay () {
-    let player1 = 0
-    let player2 = 0
-    let square1 = document.getElementById('1')
-    let square2 = document.getElementById('2')
-    let square3 = document.getElementById('3')
-    let square4 = document.getElementById('4')
-    let square5 = document.getElementById('5')
-    let square6 = document.getElementById('6')
-    let square7 = document.getElementById('7')
-    let square8 = document.getElementById('8')
-    let square9 = document.getElementById('9')
-    function winCondition (s1, s2, s3) {
-      if (s1.classList.contains('cell') && s2.classList.contains('cell') && s3.classList.contains('cell')) {
-        console.log('you win')
-      } else if (s1.classList.contains('cell2') && s2.classList.contains('cell2') && s3.classList.contains('cell2'))
-        {console.log('you win')}
-    } 
-    winCondition(square1, square2, square3)
-    winCondition(square4, square5, square6)
-    winCondition(square7, square8, square9)
-    winCondition(square1, square4, square7)
-    winCondition(square2, square5, square8)
-    winCondition(square3, square6, square7)
-    winCondition(square1, square5, square9)
-    winCondition(square7, square5, square3)
-  } 
-    
-  
-  
-  let i = true;
-  function highlight(td) {
-    selectedTd = td;
-    if (i == true){
-      selectedTd.classList.add('cell')
-      gameplay()
-      i = false;
-    } else {
-      if (selectedTd.classList.contains('cell')){return}
-      selectedTd.classList.add('cell2')
-      gameplay()
-      i = true;
+return {falsey, truey, play, incrementBlue, incrementRed, ties}
+}
+let iPlay = players();
+
+let winningCondition = () => {
+let boardArr = [];
+for (let i = 0; i <= 8; i++) {
+  boardArr.push(document.getElementById(i));
+}
+let tieCheck = (e) => {
+  let check =
+    e.classList.contains("cell") || e.classList.contains("cell2")
+      ? true
+      : null;
+  return check;
+};
+let conditions = (s1, s2, s3) => {
+  let redWins =
+    s1.classList.contains("cell") &&
+    s2.classList.contains("cell") &&
+    s3.classList.contains("cell")
+      ? true
+      : null;
+  let blueWins =
+    s1.classList.contains("cell2") &&
+    s2.classList.contains("cell2") &&
+    s3.classList.contains("cell2")
+      ? true
+      : null;
+  let tie = boardArr.every(tieCheck);
+  return { redWins, blueWins, tie };
+};
+const holder = [
+  conditions(boardArr[0], boardArr[1], boardArr[2]),
+  conditions(boardArr[3], boardArr[4], boardArr[5]),
+  conditions(boardArr[6], boardArr[7], boardArr[8]),
+  conditions(boardArr[0], boardArr[3], boardArr[6]),
+  conditions(boardArr[1], boardArr[4], boardArr[7]),
+  conditions(boardArr[2], boardArr[5], boardArr[8]),
+  conditions(boardArr[0], boardArr[4], boardArr[8]),
+  conditions(boardArr[6], boardArr[4], boardArr[2]),
+];
+const gameOver = () => {
+  for (let i = 0; i < boardArr.length; i++) {
+    boardArr[i].classList.remove("cell2");  
+    boardArr[i].classList.remove("cell");
+    iPlay.truey()
+    let rScore = document.getElementById('red');
+    let bScore = document.getElementById('blue');
+    let tScore = document.getElementById('ties');
+    rScore.innerHTML = iPlay.play.red;
+    bScore.innerHTML = iPlay.play.blue;
+    tScore.innerHTML = iPlay.play.ties;
+  }
+};
+let winner = () => {
+  for (let i = 0; i < holder.length; i++) {
+    if (holder[i].redWins) {
+      iPlay.incrementRed()
+      setTimeout(gameOver, 2500);
+      return "Red Wins!";
+    } else if (holder[i].blueWins) {
+      iPlay.incrementBlue()
+      setTimeout(gameOver, 2500);
+      return "Blue Wins!";
+    } else if (
+      holder[i].tie == true &&
+      holder[i].blueWins == null &&
+      holder[i].redWins == null
+    ) {
+      iPlay.ties()
+      setTimeout(gameOver, 2500);
+      return "Tie!";
     }
   }
-  
-  
-  
-  let grid = document.getElementById('container')
-  grid.onclick = function (event) {
-    let td = event.target.closest('div');
-    if (!td) return;
-    if (!grid.contains(td)) return;
-    highlight(td)
+};
+winner()
+};
+
+let gameBoard = () => {
+let grid = document.getElementById("container");
+let selectedTd;
+
+let highlight = (td) => {
+  selectedTd = event.target.closest("div");;
+  if (iPlay.play.truth == true) {
+    selectedTd.classList.add("cell");
+    iPlay.falsey();
+  } else {
+    if (selectedTd.classList.contains("cell")) {
+      return;
+    }
+    selectedTd.classList.add("cell2");
+    iPlay.truey();
   }
-  
-  
+};
+grid.onclick = function (event) {
+  let td = event.target.closest("div");
+  if (!td) return;
+  if (!grid.contains(td)) return;
+  if (!td.classList.contains("grid-cell")) return;
+  highlight(td)
+  winningCondition()
+};
+
+grid.onclick();
+};
+gameBoard()
+
+
+
